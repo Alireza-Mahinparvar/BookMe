1f73e851b9ca9fef1025a737f60544261a5c8eeb
domgutz3 <60630771+domgutz3@users.noreply.github.com>
2020-04-17 13:55:09 -0700

    Added register form to forms.py

--- Calendar_app/app/forms.py
+++ Calendar_app/app/forms.py
@@ -9,3 +9,20 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+    
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired(), Email(message="Please enter a valid email address")])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmPassword = PasswordField('Reenter Password', validators=[DataRequired(), EqualTo("password", message="Passwords did not match")])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username is already in use; choose another.')
+ 
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email is already in use; choose another.')
