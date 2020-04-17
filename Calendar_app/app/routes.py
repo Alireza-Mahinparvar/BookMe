d10bc16f152688c6fdf2ee703efc4a475852e3b2
domgutz3 <60630771+domgutz3@users.noreply.github.com>
2020-04-17 13:57:24 -0700

    Added register endpoint to routes.py

--- Calendar_app/app/routes.py
+++ Calendar_app/app/routes.py
@@ -10,7 +10,6 @@ from flask_login import login_required, login_user, logout_user, current_user
 def home():
     return render_template('home.html', tite = 'Home')
     
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -24,7 +23,22 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('home'))
     return render_template('login.html', title='Sign In', form=form)
-    @app.route('/logout')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Sign Up', form=form)
+
+@app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
