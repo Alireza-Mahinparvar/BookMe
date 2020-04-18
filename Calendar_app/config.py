0c0679ff1e6f82d17d9adb8afd450919d49fa11d
Benjamin Gonzalez <benjamin.gonzalez@sjsu.edu>
2020-04-17 19:54:30 -0700

    added docstrings to config.py

--- Calendar_app/config.py
+++ Calendar_app/config.py
@@ -1,7 +1,17 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config():
+    ''' Class that holds all the configuration setting for the project
+                     
+        Attributes:
+            SECRET_KEY: key used to prevent cross site attacks
+            
+            SQLALCHEMY_DATABASE_URI: location for database
+            
+            SQLALCHEMY_TRACK_MODIFICATIONS: Modification tracker                   
+    '''
     SECRET_KEY = 'you-will-never-guess'
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
