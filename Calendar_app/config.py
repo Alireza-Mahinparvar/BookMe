e8c1cb7bd05d857fd6b31e455db95d1118ea34bb
Alireza-Mahinparvar <42847715+Alireza-Mahinparvar@users.noreply.github.com>
2020-04-19 15:13:23 -0700

    Update config.py

--- Calendar_app/config.py
+++ Calendar_app/config.py
@@ -4,13 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     ''' Class that holds all the configuration setting for the project
-                     
+    
         Attributes:
             SECRET_KEY: key used to prevent cross site attacks
+            SQLALCHEMY_DATABASE_URI: location for database    
+            SQLALCHEMY_TRACK_MODIFICATIONS: Modification tracker 
             
-            SQLALCHEMY_DATABASE_URI: location for database
-            
-            SQLALCHEMY_TRACK_MODIFICATIONS: Modification tracker                   
     '''
     SECRET_KEY = 'you-will-never-guess'
 
